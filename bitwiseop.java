948a1c5377bbfe82f23f54d17d23f6450734edae
Mahalakshmi2oo4 <158253819+Mahalakshmi2oo4@users.noreply.github.com>
2025-02-12 22:20:23 +0530

    Add files via upload

---
+++ bitwiseop.java
@@ -0,0 +1,12 @@
+class bitwiseop{
+    public static void main(String args[])
+    {
+        int a=492,b=333;
+        int c=a&b;
+        int d=a|b;
+        int e=a^b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
